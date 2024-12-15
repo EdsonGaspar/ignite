f786be316e4abf19b0fcb2be62513a50fd3e328a
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-12-15 17:58:37 +0100

    Global: Instalação e configuração do chadcn na aplicação.

--- dtmoney/src/components/summary/index.tsx
+++ dtmoney/src/components/summary/index.tsx
@@ -1,7 +1,15 @@
+import { Card, CardContent, CardHeader } from "../ui/card";
+
 export function Summary() {
   return (
     <section>
       <h1>Summary</h1>
+      <Card className="text-[#C4C4CC] ">
+        <CardHeader>
+          <p>Entrada</p>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
     </section>
   );
 }
