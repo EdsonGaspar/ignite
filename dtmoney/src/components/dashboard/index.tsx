94e41cf760aed41f7312d077db15dea76621f56e
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-12-15 20:56:10 +0100

    Feat: Criando o componente do Card

--- dtmoney/src/components/dashboard/index.tsx
+++ dtmoney/src/components/dashboard/index.tsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import { Summary } from "../summary";
+interface SummaryProps {
+  titleCard: string;
+}
+const listTitleCard = ["Entrada", "Saída", "Total"];
 
 export function Dashboar() {
+  const [titleCard, setTitleCard] = useState<SummaryProps[]>([]);
+
+  useEffect(() => {
+    const formattedTitleCard = listTitleCard.map((title) => ({
+      titleFromated: title,
+    }));
+    setTitleCard(formattedTitleCard);
+  }, []);
+
   return (
-    <section className="max-w-7xl m-auto px-9">
-      <Summary />
-      <div>
-        <h1>Dashboard</h1>
-      </div>
-    </section>
+    <div className="bg-[#202024] h-screen">
+      <section className="max-w-7xl m-auto px-9">
+        <div className="flex items-center justify-between pt-10">
+          {titleCard.map((title) => (
+            <Summary titleCard={title.titleCard} />
+          ))}
+        </div>
+
+        <div>
+          <h1>Dashboard</h1>
+        </div>
+      </section>
+    </div>
   );
 }
--- dtmoney/src/components/summary/index.tsx
+++ dtmoney/src/components/summary/index.tsx
@@ -1,14 +1,19 @@
+import inputIcon from "../../assets/entrada.svg";
 import { Card, CardContent, CardHeader } from "../ui/card";
-
-export function Summary() {
+type TitleCardProps = {
+  titleCard: string;
+};
+export function Summary(title: TitleCardProps) {
   return (
     <section>
-      <h1>Summary</h1>
-      <Card className="text-[#C4C4CC] ">
+      <Card className="bg-[#323238] text-[#C4C4CC] border-none w-96 h-36 cursor-pointer">
         <CardHeader>
-          <p>Entrada</p>
+          <div className="flex  items-center justify-between">
+            <p>{title.titleCard}</p>
+            <img src={inputIcon} alt="Icon da entrada" className="w-8 h-8" />
+          </div>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent className="font-bold text-3xl">17.400,00kz</CardContent>
       </Card>
     </section>
   );
